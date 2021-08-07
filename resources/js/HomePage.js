7cf8883af645d7b5a55a6a1764e01b98fc91ddd9
Ihksans <ikhsan.setiawan.tif18@polban.ac.id>
2021-08-07 19:30:03 +0700

    deployment backend

--- resources/js/HomePage.js
+++ resources/js/HomePage.js
@@ -2,30 +2,20 @@ import React, { Component } from 'react'
 //Ini buat dependecies/library nya
 //import + "nama variabel" + from + "nama librarynya";
 
-class AdminPage extends React.Component {
+class HomePage extends React.Component {
   //deklarasi variabel
   constructor(props) {
     super()
-    this.state = {
-      a: '',
-      b: '1234',
-    }
+    this.state = {}
   }
   //contoh method
-  isiVarA() {
-    this.setState({
-      a: 'aku',
-    })
-  }
-  tambahVarB() {}
+
   render() {
     return (
-      //html
-      //js
       <>
-        <p>Ini Admin Page </p>
+        <div></div>
       </>
     )
   }
 }
-export default AdminPage
+export default HomePage
--- resources/js/Index.js
+++ resources/js/Index.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './Application';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import rootReducer from './reducers';
-import {addTokenByID} from './actions'
-import {persistor, store} from './store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Application from './Application'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { persistor, store } from './store'
 import { PersistGate } from 'redux-persist/integration/react'
-import {Provider as AuthContext} from './context/AuthContext'
 // const store = createStore(rootReducer);
 // console.log('store.getState()', store.getState());
 //  store.subscribe(()=>console.log('store', store.getState()));
 // store.dispatch(addTokenByID('xxxxxx'));
 
-ReactDOM.render(
-<Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-        <AuthContext>
-            <Application />
-        </AuthContext>
-    </PersistGate>
-   
-</Provider>
-, document.getElementById('root'));
+ReactDOM.render(<Application />, document.getElementById('root'))
